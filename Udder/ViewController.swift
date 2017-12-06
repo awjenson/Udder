93d1a87b8a28b2fe503e6885e59dfb89e35df660
AndrewJenson <awjenson@gmail.com>
2017-12-05 17:05:47 -0800

    add print statement

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Hello cow.")
+        print("test")
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +25,8 @@ class ViewController: UIViewController {
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(true)
     }
+    
+    
 
 
 }
